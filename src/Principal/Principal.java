8c87dfd670e388068d4d8f16f46fcdf33bb00146
C3za1 <C3za1.97@gmail.com>
2020-03-05 21:23:18 -0600

    GrafosInterfaz_JAVA

---
+++ src/Principal/Principal.java
@@ -0,0 +1,17 @@
+package Principal;
+
+
+import Ventanas.VentanaPrincipal;
+
+
+
+
+/**
+ *
+ * @author Raul
+ */
+public class Principal {
+    public static void main(String[] args) {
+        new VentanaPrincipal().setVisible(true);
+ }
+}
